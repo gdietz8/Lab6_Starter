eb745f7fb8c451d6b83c14a37a15b32ce4657ef7
gdietz8 <83431340+gdietz8@users.noreply.github.com>
2022-11-03 16:04:00 -0700

    data inputted for each card

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -119,28 +119,32 @@ class RecipeCard extends HTMLElement {
   set data(data) {
     // If nothing was passed in, return
     if (!data) return;
-    console.log(data);
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
     let varArticle = this.shadowRoot.querySelector('article');
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (template strings) and element.innerHTML for this.
-    varArticle.innerHTML = `<img src="https://link-to-article.com/recipe-thumbnail.jpg"
-    alt="Recipe Title">
-    <p class="title">
-      <a href="https://link-to-article.com">Title</a>
-    </p>
-    <p class="organization">The Chef's Organization</p>
-    <div class="rating">
-      <span>5</span>
-      <img src="/assets/images/icons/5-star.svg" alt="5 stars">
-      <span>(500)</span>
-    </div>
-    <time>50 min</time>
-    <p class="ingredients">
-      Comma, Separated, List, of, Ingredients
-    </p>`;
+    //console.log(data);
+    //JSON.parse(data)
+    let externalHTML = `
+      <img src= "${data.imgSrc}"
+        alt="${data.imgAlt}">
+      <p class="title">
+        <a href="${data.titleLnk}">${data.titleTxt}</a>
+      </p>
+      <p class="organization">${data.organization}</p>
+      <div class="rating">
+        <span>${data.rating}</span>
+        <img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating}">
+        <span>(${data.numRatings})</span>
+      </div>
+      <time>${data.lengthTime} min</time>
+      <p class="ingredients">
+        ${data.ingredients}
+      </p>`;
+
+    varArticle.innerHTML = externalHTML;
   }
 }
 
